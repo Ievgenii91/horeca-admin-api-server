9458e78b484f42f0a2a9b88d8677c8d8734d0de2
Ievgenii Butyrin <iebutyrin@deloitte.com>
2021-04-05 20:22:59 +0300

    prepare for 1st deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as helmet from 'helmet';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: '0712asdas3',
+      cookie: {
+        httpOnly: false,
+        sameSite: 'none',
+        secure: true,
+      },
+      resave: false,
+      saveUninitialized: true,
+    }),
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'dist'));
+
+  app.setBaseViewsDir(join(__dirname, '..', 'dist'));
+
+  app.setViewEngine('html');
+
+  app.enableCors();
+
+  app.use(helmet());
+  //router
+
   await app.listen(3000);
+
+  //socket init
+  //error
 }
 bootstrap();
