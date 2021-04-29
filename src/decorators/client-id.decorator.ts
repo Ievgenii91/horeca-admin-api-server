219ffa1b150467c1ebcf7782823c2bb32c9e66f2
Ievgenii Butyrin <iebutyrin@deloitte.com>
2021-04-29 14:34:22 +0300

    merge with bot branch -- bot disabled

--- src/decorators/client-id.decorator.ts
+++ src/decorators/client-id.decorator.ts
@@ -8,8 +8,8 @@ import { Request } from 'express';
 export const ClientId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const req = ctx.switchToHttp().getRequest<Request>();
-    if (req.headers['X-Auth-Client']) {
-      return req.headers['X-Auth-Client'] as string;
+    if (req.headers['x-auth-client']) {
+      return req.headers['x-auth-client'] as string;
     }
     if (req.params && req.params.clientId) {
       return req.params.clientId;
