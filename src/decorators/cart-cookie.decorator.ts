456a314a1b9fb2722c4869f7ac4b35283f2710ed
ievgenii <jimmydeanmaster@gmail.com>
2021-05-18 15:08:04 +0300

    cookies and search update

--- src/decorators/cart-cookie.decorator.ts
+++ src/decorators/cart-cookie.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CartId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const request = ctx.switchToHttp().getRequest();
-    return request.cookies['bc_cartId'];
+    return request.cookies['bc_cartId'] || request.query.cartId;
   },
 );
